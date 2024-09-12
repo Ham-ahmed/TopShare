6db49c862cd4d98d9cd366245dbda4f3a6b1c4d6
Ham-ahmed <106714601+Ham-ahmed@users.noreply.github.com>
2024-09-12 20:06:23 +0300

    Add files via upload

---
+++ info_topshare.sh
@@ -0,0 +1,16 @@
+echo ">>>> Top Shaer Server <<<<"
+sleep 3
+echo "> ááÊæÇÕá Úä ØÑíÞ ÇáæÇÊÓÇÈ"
+sleep 3s
+
+echo " "
+echo "> ÏÇÎá ãÕÑ"
+sleep 2s
+
+echo "> ã . ÑÈíÚ ÇáÓÇÍÑ "
+echo "> ã . ÃÈæ ÎÇáÏ "
+echo "> 01021935035 "
+sleep 2s
+
+echo ">>> Last Updatting 12-09-2024 <<<"
+exit 0
