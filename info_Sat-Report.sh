9952a1c9269702b848b0ecde02809ccb184d4527
Ham-ahmed <106714601+Ham-ahmed@users.noreply.github.com>
2024-09-12 19:54:25 +0300

    Add files via upload

---
+++ info_Sat-Report.sh
@@ -0,0 +1,149 @@
+echo ">>>> Top Shaer Server <<<<"
+sleep 3
+echo "> report for Satallite"
+sleep 3s
+
+echo " "
+echo "> Hispasat 30W"
+sleep 2s
+
+echo "> MEO TV ÇáÈÇÝÉ ÈÇáßÇãá"
+echo "> Nos ÌãíÚ ÇáÞäæÇÊ"
+echo "> MEO TV ÇáÈÇÞÉ ÈÇáßÇãá"
+sleep 2s
+
+echo " "
+echo "> Thor 1W"
+sleep 2s
+
+echo "> Focus Sat ÇáÈÇÞÉ ÈÇáßÇãá updated"
+echo "> UPC Direct ßá ÇáÞäæÇÊ ÊÚãá "
+echo "> Digi TV ÇáÈÇÞÉ ÈÇáßÇãá"
+echo "> Canal Digital ÌãíÚ ÇáÞäæÇÊ"
+sleep 2s
+
+echo " "
+echo "> Bulgaria-Sat 1.9E"
+sleep 2s
+
+echo "> Polaris Media ÌãíÚ ÇáÞäæÇÊ"
+echo "> NOS ÞäæÇÊ ãÇßÓ æßá ÈÇÞÉ"
+sleep 2s
+
+echo " "
+echo "> Nailsat 7W"
+sleep 2s
+
+echo "> ART ÞäæÇÊ ÈÇÞÉ"
+sleep 2s
+
+echo " "
+echo "> Amos 4W"
+sleep 2s
+
+echo "> ÇÞÉ YES ÈÇáßÇãá SD,HD,4K"
+sleep 2s
+
+echo " "
+echo "> Eutlsat 3E"
+sleep 2s
+
+echo "> mideasat ÇáÇíØÇáíå ßÇãáå"
+sleep 2s
+
+echo " "
+echo "> Eutlsat 9E"
+sleep 2s
+
+echo "> KABILE ÇáÇáãÇäíÉ ßÇãáå ãÊæÞÝå ÍÇáíÇ"
+echo "> discovery ÇáÇáãÇäíÉ ÇáãÊÑÌãå ááÚÑÈíÉ ÍÕÑíÇ ßÇãáå"
+echo "> AFN ßÇãáå ÊÚãá Úáí ÃÌåÒÉ ÇáÌíá ÇáËÇáË "
+sleep 2s
+
+echo " "
+echo "> Hotbird 13E"
+sleep 2s
+
+echo "> Eurosport ÇáÞäæÇÊ ÇáÇäÌáíÒíÉ"
+echo "> NC+ ÈÇáßÇãá ãä ÖãäåÇ ÞäæÇÊ Eleven - Canal+ "
+echo "> Polsat Sport Premium æ Cyfrowy Polsat ÞäæÇÊ ÈÇÞÉ "
+echo "> Mediaset ÊÚãá ÈÇáßÇãá ÈÇÞÉ "
+echo "> SRG SSR ÊÚãá ÈÇáßÇãá ÈÇÞÉÇáÓæíÓÑíÉ "
+echo "> RAI ÇáÞäæÇÊ ÇáÇíØÇáíÉ ÈÇáßÇãá"
+echo "> Love Nature 4k æ My zen 4K æ Museum 4K ÊÚãá ÈÇãÊíÇÒ"
+echo "> AB ÇáÈÇÞÉ ÇáÝÑäÓíÉ Action æÞäÇÉ"
+echo "> HBO ßÇãá ÇáÞäæÇÊ "
+echo "> VOD ßÇÝÉ ÞäæÇÊ ÈÇÞÉ "
+echo "> Travelxp 4k ÞäÇÉ "
+echo "> vivacom ÞäæÇÊ "
+sleep 2s
+
+echo " "
+echo "> Eutelsat 16E"
+sleep 2s
+
+echo "> MAX TV ÇáÈÇÞÉ ÇáßÑæÇÊíÉ"
+echo "> Antiksat ÌãíÚ ÞäæÇÊ ÈÇÞÉ "
+echo "> Pink ÈÇÞÉ ÌãíÛ ÞäæÇÊ "
+echo "> HRT ÇáÈÇÞÉ ÇáßÑæÇÊíÉ"
+echo "> Hayat HD ÝäæÇÊ ÈÇÞÉ "
+echo "> Freesat Romania æ Sport.Ro æ Pro TV æ Kanal D Romania æÞäæÇÊ TVR"
+sleep 2s
+
+echo " "
+echo "> Astra 19.2E"
+sleep 2s
+
+echo "> SKYDE ÇáÈÇÞÉ ÇáÇáãÇäíÉ"
+echo "> + Astra ÞäæÇÊ "
+echo "> HD+ ÞäæÇÊ ÈÇÞÉ "
+echo "> TNT ÞäæÇÊ ÈÇÞÉ "
+echo "> ORF  ÞäæÇÊ ÈÇÞÉ "
+sleep 2s
+
+echo " "
+echo "> Astra 23E"
+sleep 2s
+
+echo "> CanalDigitaal ÞäæÇÊ "
+echo "> Skylink ÞäæÇÊ ÈÇÞÉ "
+sleep 2s
+
+echo " "
+echo "> Eutelsat 36E"
+sleep 2s
+
+echo "> NTV Plus ÞäæÇÊ ÇáÈÇÞÉ "
+sleep 2s
+
+echo " "
+echo "> Hellasat 39E"
+sleep 2s
+
+echo "> Bulsatcom ÞäæÇÊ ÈÇÞÉ "
+echo "> telekome romania ÞäæÇÊ ÈÇÞÉ "
+sleep 2s
+
+echo " "
+echo "> TurkSat 42E "
+sleep 2s
+
+echo "> Dsmart æÇÛáÈ ÞäæÇÊ ÇáÞãÑ "
+sleep 2s
+
+echo " "
+echo "> ÇáÞãÑ ÇáÇÒÑí"
+sleep 2s
+
+echo "> vivacom æÊÚãá ÇÛáÈ ÞäæÇÊ ÇáÞãÑ"
+sleep 2s
+
+echo " "
+echo "> Turkmani + yahsat 52E "
+sleep 2s
+
+echo "> Big BANG ÊÚãá ÈÇãÊíÇÒ"
+sleep 2s
+
+echo ">>> Last Updatting 12-09-2024 <<<"
+exit 0
