92cda63ea680a5b08bd5a58eb98cc8905bdd36c5
Ham-ahmed <106714601+Ham-ahmed@users.noreply.github.com>
2024-09-14 23:09:33 +0300

    Update info_topshare.sh

--- info_topshare.sh
+++ info_topshare.sh
@@ -26,5 +26,13 @@ echo "> متوفر تجديد فوريفر "
 echo "> تواصل علي الواتساب "
 sleep 2s
 
+echo " "
+echo "> 14-09-2024"
+sleep 2s
+
+echo "> TopShare Server oscam No.1 & No.3 Test 7 days Free 14-9-2024 updated"
+echo "> TopShare Server Ncam No.2 & No.4 Test 7 days Free 14-9-2024 updated"
+sleep 2s
+
 echo ">>> Last Updatting 12-09-2024 <<<"
 exit 0
